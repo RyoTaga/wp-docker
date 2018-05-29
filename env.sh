668358b6c2fe0c9ae1556d5fc56fcc2cfe4cb498
RyoTaga <taga@n-di.co.jp>
2018-05-29 19:43:16 +0900

    エイリアスを利用できるようにした。

---
+++ env.sh
@@ -0,0 +1,5 @@
+alias wpdb='docker exec -it wp-db mysql -p -u root'
+alias wpdbdump='docker exec -it wp-db mysqldump -p -u root wp_db'
+
+alias wpdbbash='docker exec -it wp-db bash'
+alias wpwebbash='docker exec -it wp-web bash'
